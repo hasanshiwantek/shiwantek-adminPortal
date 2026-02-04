998aec835ce71db33b73292ce7bd0eb6544c582c
hasanshiwantek <hasan.shiwantek@gmail.com>
2026-02-05 04:01:06 +0500

    Added endpoint

--- vite.config.js
+++ vite.config.js
@@ -6,37 +6,37 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      "/auth": {
-        target: "https://operations.advertsedge.com/api",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/orders": {
-        target: "https://operations.advertsedge.com/api",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/users/index": {
-        target: "https://operations.advertsedge.com/api",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/my-sheet-data": {
-        target: "https://operations.advertsedge.com/api",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/my-sheet-order?order_id=": {
-        target: "https://operations.advertsedge.com/api",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/my-sheet-data?sheet_id=": {
-        target: "https://operations.advertsedge.com/api",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    // proxy: {
+    //   "/auth": {
+    //     target: "https://operations.advertsedge.com/api",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    //   "/orders": {
+    //     target: "https://operations.advertsedge.com/api",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    //   "/users/index": {
+    //     target: "https://operations.advertsedge.com/api",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    //   "/my-sheet-data": {
+    //     target: "https://operations.advertsedge.com/api",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    //   "/my-sheet-order?order_id=": {
+    //     target: "https://operations.advertsedge.com/api",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    //   "/my-sheet-data?sheet_id=": {
+    //     target: "https://operations.advertsedge.com/api",
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    // },
   },
 });
